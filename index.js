532607ebed470285693f7482d1a4fdfc8c14869d
itsupport-web <itsupport@athrotech.com>
2026-02-04 13:56:14 +0800

    Create index.js

---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const app = express();
+app.use(express.static("public"));
+app.use(express.json())
+app.post("/update", (req, res) => {
+  if(req.get('Referrer') == "https://color-catch.glitch.me/"){
+  console.log("sent")
+  fs.readFile("leaderboard.txt", 'utf8', (err, data) => {
+    let datas = data.split("\n")
+    datas.forEach((e,i)=>{
+      datas[i] = e.split(" | ")[0]
+    })
+    res.send(datas)
+  })
+  }else{
+     res.send("baka desu yooooooooo")
+  }
+});
+app.listen(3000, () => {
+  console.log(`Server is running on port ${3000}`);
+}); 
