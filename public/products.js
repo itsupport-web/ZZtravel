c37164979c5459fa6b82fc68f437f0eeeb32f266
itsupport-web <itsupport@athrotech.com>
2026-02-05 15:00:51 +0800

    ff

---
+++ public/products.js
@@ -0,0 +1,16 @@
+fetch("/products")
+  .then(res => res.json())
+  .then(rows => {
+    console.log(rows)
+    rows.forEach(product => {
+      const el = document.createElement("div");
+
+      el.innerHTML = `
+        <p>${product.id}</p>
+        <p>${product.name}</p>
+        <p>${product.description}</p>
+      `;
+
+      document.getElementById("product").appendChild(el);
+    });
+  });
