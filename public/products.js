d3048293e754206c74b4317e549256f2569f7442
itsupport-web <itsupport@athrotech.com>
2026-02-06 16:34:27 +0800

    l

--- public/products.js
+++ public/products.js
@@ -8,13 +8,11 @@ fetch("/products")
             const url = `./productdetail.html?id=${product.id}&name=${encodeURIComponent(product.name)}&desc=${encodeURIComponent(product.description)}`;
             window.location.href = url;
         };
-
-      el.innerHTML = `
-        <p>ID : ${product.id}</p>
-        <p>NAME : ${product.name}</p>
-        <p>DESCRIPTION : ${product.description}</p>
-      `;
-
+        el.innerHTML = `
+          <p>ID : ${product.id}</p>
+          <p>NAME : ${product.name}</p>
+          <p>DESCRIPTION : ${product.description}</p>
+        `;
       document.getElementById("products").appendChild(el);
     });
   });
