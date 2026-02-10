02092b84c8423deb5a42a50ff65f1932eef04cca
itsupport-web <itsupport@athrotech.com>
2026-02-10 14:48:24 +0800

    d

--- public/signin/signin.js
+++ public/signin/signin.js
@@ -1,10 +1,1 @@
-const loginForm = document.getElementById("loginForm");
-
-loginForm.addEventListener('submit', e => {
-  e.preventDefault(); // stops redirect
-  fetch('/users/check', {
-    method: 'POST',
-    body:"testing"
-  })
-});
 
