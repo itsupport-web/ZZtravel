7a5b4d60b3e4e0fa156ea194ffcbf0e045b77884
itsupport-web <itsupport@athrotech.com>
2026-02-10 12:52:07 +0800

    c

---
+++ public/signin/signin.js
@@ -0,0 +1,9 @@
+document.getElementById("loginForm").addEventListener('submit', e => {
+  e.preventDefault(); // stops redirect
+  fetch('/users/check', {
+    method: 'POST',
+    body: new FormData(loginForm)
+  })
+  window.location.href = "/admin";
+});
+
