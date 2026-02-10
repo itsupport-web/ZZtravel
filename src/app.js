27fea9c688261130554b300b7029e7541792c054
itsupport-web <itsupport@athrotech.com>
2026-02-10 09:49:33 +0800

    d

--- src/app.js
+++ src/app.js
@@ -14,3 +14,7 @@ app.use(session({
 
 const userRoutes = require('./route/userRoutes'); 
 app.use('/users', userRoutes);
+
+app.listen(3000, () => {
+  console.log(`Server running on port ${3000}`);
+});
