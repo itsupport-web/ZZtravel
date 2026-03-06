8c3418308c126ed58a43183c5620b6045fa8fbea
itsupport-web <itsupport@athrotech.com>
2026-03-06 16:04:20 +0800

    s

--- src/controller/eventController.js
+++ src/controller/eventController.js
@@ -22,10 +22,14 @@ async function getImage(req, res){
   try {
     const file = await eventService.getImage(req.query.id);
     res.set('Content-Type', file.type);
+    res.set('Cache-Control', 'no-store');
+    res.set('Pragma', 'no-cache');
+    res.set('Expires', '0');
+    res.removeHeader('ETag');
     res.send(file.data);
   } catch (err) {
     console.error("error getting image: ", err);
-    res.send('Image not found');
+    res.status(404).send('Image not found');
   };
 }
 
