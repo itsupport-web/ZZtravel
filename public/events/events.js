c99168316ed3b9add1165f403a58cad348a6b1f4
itsupport-web <itsupport@athrotech.com>
2026-03-13 17:59:17 +0800

    s

--- public/events/events.js
+++ public/events/events.js
@@ -110,32 +110,32 @@ function filter(){
   fetch(url,{method : "GET"})
   .then(res => res.json())
   .then(rows => {
-    showEvents(rows)  
+    showEvents(rows)
   })
 }
+
 async function showEvents(rows){
   document.getElementById("events").innerHTML = "";
   if(rows.length == 0){
     document.getElementById("events").innerHTML = "No Events Yet";
     return;
   }
-  rows.forEach(async (event) => {
+  rows.forEach((event) => {
     const dateArray = formatDateString(event.event_date);
-    const imageSource = await requestImage(event.image);
     const el = document.createElement("div");
     el.classList.add("event-card");
     el.innerHTML = `
         <div class = "event-image">
-          <p class = "event-rotated-text">${event.status}<p>
-          <img src = "${imageSource}">
+          <p class = "event-rotated-text">${event.status}</p>
+          <img src="/events/getimage?id=${event.image}">
         </div>
         <div class = "event-content">
-          <h1${event.title}</h1>
+          <h1>${event.title}</h1>
           <p>${event.description}</p>
         </div>
         <div class = "event-date">
-          <h1 style = "font-size : 20px">${dateArray[0]}</h1>
-          <h1 style = "font-size : 70px ; color :#EC3669">${dateArray[1]}</h1>
+          <h1 style = "font-size : 20px">${dateArray[1]}</h1>
+          <h1 style = "font-size : 70px ; color :#EC3669">${dateArray[0]}</h1>
           <h1 style = "font-size : 20px">${dateArray[2]}</h1>
         </div>
       `;
